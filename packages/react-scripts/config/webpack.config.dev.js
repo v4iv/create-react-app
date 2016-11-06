d3aff974afb6581a36e622ea1ee66e0ea99e00b8
Vaibhav Sharma <vaibhav_sharma@outlook.in>
2016-11-06 14:49:12 +0530

    Added SCSS loader support

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -135,6 +135,12 @@ module.exports = {
         test: /\.css$/,
         loader: 'style!css?importLoaders=1!postcss'
       },
+      // "scss" loader resolves paths in scss and adds assets as dependencies
+      {
+        test: /\.scss$/,
+        include: paths.appSrc,
+        loaders: ["style", "css", "scss"]
+      },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
       {
